4a6c17a7ab49f5c9f86b569d6479e636ac4de49f
lidahe <lidahe@wsdashi.com>
2017-01-27 16:46:55 +0800

    add Fruit

---
+++ cpp/CppTest/CppTest/Fruit.cpp
@@ -0,0 +1,25 @@
+//
+//  Fruit.cpp
+//  CppTest
+//
+//  Created by lidahe on 17/1/27.
+//  Copyright © 2017年 lidahe. All rights reserved.
+//
+
+#include "Fruit.h"
+#include <iostream>
+
+using namespace std;
+
+
+int main() {
+    
+//    Fruit fruit;
+//    Apple apple;
+    
+    cout << "int:" << sizeof(int) << " double:" << sizeof(double) << " char:" << sizeof(char) << endl;
+    cout << "Fruit:" << sizeof(Fruit) << endl;
+//    cout << "Apple:" << sizeof(Apple) << endl;
+    cout << "Pear:" << sizeof(Pear) << endl;
+    return 0;
+}
---
+++ cpp/CppTest/CppTest/Fruit.h
@@ -0,0 +1,37 @@
+//
+//  Fruit.hpp
+//  CppTest
+//
+//  Created by lidahe on 17/1/27.
+//  Copyright © 2017年 lidahe. All rights reserved.
+//
+
+#ifndef Fruit_hpp
+#define Fruit_hpp
+
+#include <stdio.h>
+
+
+class Fruit {
+    int no;
+    double weight;
+    char key;
+//public:
+//    void print() {   }
+//    virtual void process(){   }
+};
+
+//class Apple: public Fruit{
+//    int size;
+//    char type;
+//public:
+//    void save() {   }
+//    virtual void process(){   }
+//};
+
+class Pear {
+    int no;
+    char key;
+    double weight;
+};
+#endif /* Fruit_hpp */
